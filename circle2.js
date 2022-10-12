9b67aa245f65e2baa76b2d78285335b162c3b3e7
dibashmgr <115499557+dibashmgr@users.noreply.github.com>
2022-10-12 14:59:16 -0400

    U D

--- circle2.js
+++ circle2.js
@@ -8,15 +8,24 @@ function setup() {
 px = 0
 py = 0
 
-vx = 0
-vy = 0
+vx = 1
+vy = 1
+
 radius = 30
+
 function draw () {
     background(0)
+    px = px + vx
+    py = py + vy
 
-    vx = vx + 0.5
-    vy = vy + 0.5
+    if (px > 600 || px  <0){
+        vx = -vx
+    }
 
+    if (py > 400 || py < 0){
+        vy = -vy
+    } 
 
-    ellipse(px+vx, radius, radius);
+    
+    ellipse(px, py, radius, radius);
 }
